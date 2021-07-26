b61848bc679308e5e778f77c8934d4065f3c65e5
afiq naqiudean <afiqnaqiuddean@gmail.com>
2021-07-26 17:59:02 +0800

    adding 1 new files

---
+++ Homework1-solution.py
@@ -0,0 +1,58 @@
+beeps = 2
+boops = 6
+total = beeps + boops
+print(total)
+8
+beeps1 = 2
+boops1 = 6
+total1 = beeps1 + boops1
+beeps2 = 0
+boops2 = 5
+total2 = beeps2 + boops2
+beeps3 = 9
+boops3 = 3
+total3 = beeps3 + boops3
+beeps4 = 4
+boops4 = 8
+total4 = beeps4 + boops4
+beeps5 = 10
+boops5 = 5
+total5 = beeps5 + boops5
+
+
+beeps6 = 11
+boops6 = 12
+total6 = beeps6 + boops6
+beeps7 = 10
+boops7 = 5
+total7 = beeps7 + boops7
+beeps8 = 1
+boops8 = 17
+total8 = beeps8 + boops8
+beeps9 = 5
+boops9 = 7
+total9 = beeps9 + boops9
+beeps10 = 4
+boops10 = 0
+total10 = beeps10 + boops10
+print(total1) # H
+print(total2) # E
+print(total3) # L
+print(total4) # L
+print(total5) # o
+print(total6) # W
+print(total7) # o
+print(total8) # R
+print(total9) # L
+print(total10) # D
+
+8
+5
+12
+12
+15
+23
+15
+18
+12
+4
